5bbe8effa6c9a9e794aed0d7777e111a0835a734
Marta <mlopron829@g.educaand.es>
2025-01-29 12:38:43 +0100

    ej 3

--- clase/2SMRA/DOM/Modelo/script.js
+++ clase/2SMRA/DOM/Modelo/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    /*--------------- Pruebas iniciales de JavaScript ------------*/
     /* Seleccionar un unico elemento */
     console.log(document.getElementsByTagName("h1")); /*Seleccionando por etiqueta*/
     console.log(document.getElementsByClassName("main__title")); /*Seleccionando por clase*/
@@ -26,30 +27,69 @@ document.addEventListener("DOMContentLoaded", function(){
 
         /*EJ1:Seleccionamos variable titulo y le modificamos el contenido del texto*/
         let titulo = document.querySelector("h1");
-        titulo.textContent = "Hola mundo con JavaScript";
-
+        if(titulo.textContent == "Hola mundo con JavaScript"){
+            titulo.textContent = "Hola mundo"; 
+        }else{
+            titulo.textContent = "Hola mundo con JavaScript";
+        }
+      
         /*EJ2:Seleccionamos variable titulo y le modificamos las propiedades de estilo (css)*/
         /* Forma 1: accediendo directamente a las propiedades*/
         // titulo.style.color ="red";
         // titulo.style.textAlign= "center";
 
         /* Forma 2: creando una clase nueva y añadiendola a nuestra etiqueta*/
-        titulo.classList.add("main__title--change");
+        titulo.classList.toggle("main__title--change");
     }
+
+
+    /*Ejercicio 3*/
+    function creaDiv(){
+
+        let div = document.createElement("div"); //Creo el elemento
+        div.textContent = "Javascript permite crear páginas dinámicas"; //Modifico el contenido
+        div.classList.add("texto"); //Añadimos el estilo (clase de css)
+
+        console.log(div);
+
+        // Posicionamos nuestro nuevo elemento dentro de nuestro documento
+        // Lo ponemos al final de nuestro main
+        // const main = document.querySelector("main");
+        // main.appendChild(div);
+
+        // Lo ponemos detras del titulo principal
+        const titulo = document.querySelector("h1");
+        titulo.parentNode.insertBefore(div, titulo.nextSibling);
+    }
+
 
     /* ------ Parte principal de mi codigo -----*/
     const btn_tit = document.querySelector(".header__btn--change");
-    /* Ejercicio 1*/
+    const btn_blq = document.querySelector(".header__btn--create");
+
+
+    /* Ejercicio 1 y 2*/
     btn_tit.addEventListener("click", function(){
 
         const titulo = document.querySelector("h1");
-        if(titulo.classList.contains("main__title--change")){
-            titulo.classList.remove("main__title--change");
-            titulo.textContent = "Hola mundo";
-        }else{
-            cambiar();
-        }
-        
+        // Otra forma alternativa de hacer un toggle manual
+        // if(titulo.classList.contains("main__title--change")){
+        //     titulo.classList.remove("main__title--change");
+        //     titulo.textContent = "Hola mundo";
+        // }else{
+        //     cambiar();
+        // }
+
+        /*Modificamos el contenido y el estilo del titulo*/
+        cambiar();
+        /*Modificando el estilo del boton al pulsar click*/
+        btn_tit.classList.toggle("header__btn--click");
+
+    });
+
+    btn_blq.addEventListener("click", function(){
+        console.log("entra");
+        creaDiv();
     });
 
 
