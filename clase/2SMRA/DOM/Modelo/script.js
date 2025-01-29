51d4ce75d7f592d2efd7065aea6a30676c9dda0e
Marta <mlopron829@g.educaand.es>
2025-01-29 11:54:59 +0100

    ej1 y 2

--- clase/2SMRA/DOM/Modelo/script.js
+++ clase/2SMRA/DOM/Modelo/script.js
@@ -16,7 +16,42 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log(titulo); /*Devuelve el objeto*/
     console.log(titulo.textContent); /*Devuelve el valor del objeto*/
     console.log(titulo.innerHTML); /*Devuelve el valor del objeto*/
-    titulo.textContent = "Hola mundo con JavaScript"; /*Modifico el valor del objeto*/
+    // titulo.textContent = "Hola mundo con JavaScript"; /*Modifico el valor del objeto*/
+    // titulo.innerHTML = "<br> <br> <em> Hola mundo con JavaScript </em>";
+
+    /* --------------- Creacion de funciones -----------------*/
+
+    /* Ejercicio 1: Creacion de nuestro primer evento */
+    function cambiar(){
+
+        /*EJ1:Seleccionamos variable titulo y le modificamos el contenido del texto*/
+        let titulo = document.querySelector("h1");
+        titulo.textContent = "Hola mundo con JavaScript";
+
+        /*EJ2:Seleccionamos variable titulo y le modificamos las propiedades de estilo (css)*/
+        /* Forma 1: accediendo directamente a las propiedades*/
+        // titulo.style.color ="red";
+        // titulo.style.textAlign= "center";
+
+        /* Forma 2: creando una clase nueva y añadiendola a nuestra etiqueta*/
+        titulo.classList.add("main__title--change");
+    }
+
+    /* ------ Parte principal de mi codigo -----*/
+    const btn_tit = document.querySelector(".header__btn--change");
+    /* Ejercicio 1*/
+    btn_tit.addEventListener("click", function(){
+
+        const titulo = document.querySelector("h1");
+        if(titulo.classList.contains("main__title--change")){
+            titulo.classList.remove("main__title--change");
+            titulo.textContent = "Hola mundo";
+        }else{
+            cambiar();
+        }
+        
+    });
+
 
 
 });
