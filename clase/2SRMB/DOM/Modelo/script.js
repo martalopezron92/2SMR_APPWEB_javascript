4c57d1390d1481c7530cc8dd33500734f3d20211
mlopron829 <mlopron829@g.educaand.es>
2025-01-30 12:47:20 +0100

    nuevos cambios 01

--- clase/2SRMB/DOM/Modelo/script.js
+++ clase/2SRMB/DOM/Modelo/script.js
@@ -7,3 +7,16 @@ console.log(document.getElementById("titulo")); // por id
 console.log(document.querySelector("h1")); // por etiqueta
 console.log(document.querySelector(".main__title")); //por clase
 console.log(document.querySelector("#titulo")); //por id
+
+/* Seleccionamos multiples objetos*/
+console.log(document.querySelector(".header__btn")); // Solo se queda con el primero 
+console.log(document.querySelectorAll(".header__btn")); // Me devuelve un array de objetos
+console.log(document.getElementsByClassName("header__btn")); // Me devuelde un array de objetos
+
+
+/* EJ1: Vamos a aprender a seleccionar y modificar un elemento en concreto*/
+let titulo = document.querySelector("h1");
+console.log(titulo.textContent) // Visualizamos el contenido de texto de nuestro objeto
+titulo.textContent = "Hola mundo con JavaScript"; // Modificamos el valor del objeto
+console.log(titulo.innerHTML);
+
