640107ca7aeed4b105c8dfc647196ca3be72566b
mlopron829 <mlopron829@g.educaand.es>
2025-01-30 13:30:05 +0100

    pomodoro 4 2smrb

--- clase/2SRMB/DOM/Modelo/script.js
+++ clase/2SRMB/DOM/Modelo/script.js
@@ -20,15 +20,29 @@ function cambiar(){
 
     /* EJ1*/
     let titulo = document.querySelector("h1");
+    const contenido = titulo.textContent;
     console.log(titulo.textContent) // Visualizamos el contenido de texto de nuestro objeto
-    titulo.textContent = "Hola mundo con JavaScript"; // Modificamos el valor del objeto
+    if(contenido == "Hola mundo con JavaScript"){
+        titulo.textContent = "Hola mundo";
+    }else{
+        titulo.textContent = "Hola mundo con JavaScript"; // Modificamos el valor del objeto
+    }
     
     // Otra manera de acceder y modificar el contenido de un objeto*/
     // console.log(titulo.innerHTML); // Accedemos al contenido del objeto
     // titulo.innerHTML = "<br><br><br><br><em> Hola mundo <em>"; // Admite e interpreta etiquetas html
 
     /* EJ2*/
-    
+    /* Una forma de cambiar directamente las propiedades de estilo de un objeto -- No recomendada*/
+    // titulo.style.color = "red";
+    // titulo.style.textAlign = "center";
+    // titulo.style.backgroundColor ="yellow";
+
+    /*Otra forma de hacerlo: crearnos una clase en css con las propiedades y añadir esta clase
+    a nuestro objeto*/
+    titulo.classList.toggle("main__title--change");
+
+
 }
 
 /* Para trabajar con eventos, vamos a utilizar la funcion addEventListener*/
