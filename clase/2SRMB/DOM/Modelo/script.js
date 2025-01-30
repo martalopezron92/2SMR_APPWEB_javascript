0908b93db686367927f9741506fbde36640d67f4
mlopron829 <mlopron829@g.educaand.es>
2025-01-30 12:27:20 +0100

    nuevo modelo 2srmb

---
+++ clase/2SRMB/DOM/Modelo/script.js
@@ -0,0 +1,9 @@
+
+/* Vamos a aprender a seleccionar elementos en js*/
+console.log(document.getElementsByTagName("h1")); // por etiqueta
+console.log(document.getElementsByClassName("main__title")); // por clase
+console.log(document.getElementById("titulo")); // por id
+
+console.log(document.querySelector("h1")); // por etiqueta
+console.log(document.querySelector(".main__title")); //por clase
+console.log(document.querySelector("#titulo")); //por id
