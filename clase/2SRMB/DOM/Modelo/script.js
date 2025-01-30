93a609c8aea51f0d5c0442b7f75faf3437fc4cae
mlopron829 <mlopron829@g.educaand.es>
2025-01-30 14:09:29 +0100

    pomodoro 5 2srmb

--- clase/2SRMB/DOM/Modelo/script.js
+++ clase/2SRMB/DOM/Modelo/script.js
@@ -1,4 +1,6 @@
 
+/*------------------------- Introduccion ------------------------------*/
+
 /* Vamos a aprender a seleccionar elementos en js*/
 console.log(document.getElementsByTagName("h1")); // por etiqueta
 console.log(document.getElementsByClassName("main__title")); // por clase
@@ -13,6 +15,8 @@ console.log(document.querySelector(".header__btn")); // Solo se queda con el pri
 console.log(document.querySelectorAll(".header__btn")); // Me devuelve un array de objetos
 console.log(document.getElementsByClassName("header__btn")); // Me devuelde un array de objetos
 
+
+/* ------------------------ Funciones ------------------------------*/
 
 /* EJ1: Vamos a aprender a seleccionar y modificar un elemento en concreto*/
 /* EJ2: Vamos a aprender a cambiar las propiedades css de nuestro objeto (estilos)*/
@@ -41,13 +45,42 @@ function cambiar(){
     /*Otra forma de hacerlo: crearnos una clase en css con las propiedades y añadir esta clase
     a nuestro objeto*/
     titulo.classList.toggle("main__title--change");
+}
+
+// EJ3: Vamos a aprender a crear nuevos elementos y objetos dentro de mi html
+function crearDiv(){
+
+    const new_ele = document.createElement("div"); // Creamos un nuevo elemento
+    new_ele.textContent = " Javascript permite crear páginas dinámicas."; //Añadimos contenido
+    new_ele.classList.add("texto"); // Añadimos el estilo
+
+    // Posicionamos nuestro elemento detras del ultimo hijo de nuestra etiqueta padre
+    // const padre = document.querySelector("main");
+    // padre.appendChild(new_ele);
+
+    // Posicionamos nuestro elemento detras de la etiqueta h1
+    const titulo = document.querySelector("h1"); // elemento de referencia
+    titulo.parentNode.insertBefore(new_ele, titulo.nextSibling)
+
 
 
 }
 
+
+/*---------------------- Eventos y parte principal del codigo ---------------*/
+
 /* Para trabajar con eventos, vamos a utilizar la funcion addEventListener*/
 const btn_tit = document.querySelector(".header__btn--change");
+const btn_blq = document.querySelector(".header__btn--create");
 
 btn_tit.addEventListener("click", function(){
+    /* Cambiar el contenido y la apariencia (css) de mi etiqueta*/
     cambiar();
+    /* Cambiar la apariencia de mi boton*/
+    this.classList.toggle("header__btn--click");
 });
+
+btn_blq.addEventListener("click", function(){
+    crearDiv();
+});
+
